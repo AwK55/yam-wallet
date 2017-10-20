11bc5f90af844956509d855180230451e10c51d3
Andrei Kiselev <aw.v.kiselev@gmail.com>
2017-10-20 09:20:26 +0300

    add https

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const middlewares = require('./app/middlewares/index');
 const config = require('./config/index');
 const logger = require('./utils/logService')('app');
 
-module.exports.load = function () {
 
-  const app = new Koa();
 
-  app.name = config.main.name;
-  app.env = config.main.env;
+const app = new Koa();
 
-  app.use(middlewares());
 
-  app.listen(3000, () => {
-    logger.info('YM Node School App listening on port 3000!');
-  });
-}
+global.appConfig = config;
+app.name = config.main.name;
+app.env = config.main.env;
+
+
+app.use(middlewares());
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const app = require('./app');
 
-app.load();
+
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('../'),
+  cert: fs.readFileSync('test/fixtures/keys/agent2-cert.pem')
+};
+
+https.createServer(options, app.callback()).listen(3005);
+
+app.listen(3000, () => {
+  logger.info('YM Node School App listening on port 3000!');
+});;
